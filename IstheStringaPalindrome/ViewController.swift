56b2f61d68fa8c51ce8f2e7c7968c11866e26b31
Daniel Washington <danielvertigo@hotmail.com>
2021-06-15 17:43:18 -0300

    Initial Commit

---
+++ IstheStringaPalindrome/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IstheStringaPalindrome
+//
+//  Created by Daniel Washington Ignacio on 15/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
